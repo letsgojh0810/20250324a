c732b703373a5c516cfb53cb46ab6250ef0b0d1f
letsgojh0810 <han950717@gmail.com>
2025-04-09 11:10:10 +0900

    feat

--- step07_CICD/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
+++ step07_CICD/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
@@ -1,13 +1,20 @@
 package edu.fisa.ce;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Step07CicdApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Test // JUnit 4
+	// @org.junit.jupiter.api.Test // JUnit 5
+	public void testAddition() {
+		int result = 2 + 3;
+		System.out.println("비교 전");
+		assertEquals(5, result); // 성공: 5 == 5
+		System.out.println("비교 후");
 	}
 
 }
